f759cf4249eb578769c0bdaafae357c6b80615fb
Austin Ziegler <austin@zieglers.ca>
2019-04-03 16:46:38 -0400

    Use interning for value pool

--- lib/mime/type/value_pool.rb
+++ lib/mime/type/value_pool.rb
@@ -1,13 +1,30 @@
 # frozen_string_literal: true
 
 MIME::Type::ValuePool = Hash.new { |h, k|
-  begin
-    k = k.dup
-  rescue TypeError
-    k
-  else
-    k.freeze
-  end
+  k =
+    if k.kind_of?(String)
+      MIME::Type::ValuePool.intern_string(k)
+    else
+      begin
+        k.dup
+      rescue TypeError
+        k
+      else
+        k.freeze
+      end
+    end
 
   h[k] = k
 }
+
+class << MIME::Type::ValuePool
+  if String.method_defined?(:-@)
+    def intern_string(string)
+      -string
+    end
+  else
+    def intern_string(string)
+      string.freeze
+    end
+  end
+end
